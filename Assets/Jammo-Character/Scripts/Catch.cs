1723a63b6b1209a427927f3890caf118a3b38789
nekotom2 <thomas.sanchezs@utadeo.edu.co>
2023-05-10 03:25:58 -0500

    Sistema de agarrar y soltar

---
+++ Assets/Jammo-Character/Scripts/Catch.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Catch : MonoBehaviour
+{
+
+    public GameObject handPoint;
+    
+    private GameObject pickedObject = null;
+   
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+        if(pickedObject != null){
+
+            if(Input.GetKey("r")){
+
+                pickedObject.GetComponent<Rigidbody>().useGravity = true;
+                pickedObject.GetComponent<Rigidbody>().isKinematic = false;
+                pickedObject.gameObject.transform.SetParent(null);
+                pickedObject = null;
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other) {
+
+        if (other.gameObject.CompareTag("Collectibles")) {
+
+            if(Input.GetKey("e") && pickedObject == null){
+
+                other.GetComponent<Rigidbody>().useGravity = false;
+                other.GetComponent<Rigidbody>().isKinematic = true;
+                other.transform.position = handPoint.transform.position;
+                other.gameObject.transform.SetParent(handPoint.gameObject.transform);
+                pickedObject = other.gameObject;
+            }
+        }
+    }
+}
