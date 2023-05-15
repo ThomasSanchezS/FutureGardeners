f0eef52a740a261f3384e61089c44336094a8752
nekotom2 <thomas.sanchezs@utadeo.edu.co>
2023-05-14 22:44:55 -0500

    UI en scena de angel modificado script game manager

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,8 +11,9 @@ public class GameManager : MonoBehaviour
 
     public int player1Points, player2Points;
     private int bluePlantPoints, whitePlantPoints, redPlantPoints;
+    
 
-    public TextMeshProUGUI timerText, blueText, whiteText, redText;
+    public TextMeshProUGUI timerText, blueText, whiteText, redText, points1Text, points2Text, changePointText, startText;
 
     private void Start()
     {
@@ -20,6 +22,7 @@ public class GameManager : MonoBehaviour
         player2Points = 0;
         FindNewPlantPoints();
         UpdatePlantPoints();
+        StartAnimation();
     }
 
     void Update()
@@ -27,8 +30,19 @@ public class GameManager : MonoBehaviour
         timer-= Time.deltaTime;
 
         timerText.text = ((int)timer).ToString() + " Sec";
+        points1Text.text = "P1: " + ((int)player2Points).ToString();
+        points2Text.text = "P2: " + ((int)player2Points).ToString();
 
         ChangePlantPoints();
+    }
+
+    private void GameOver(){
+        if(timer == 0 && player1Points > player2Points)
+        SceneManager.LoadScene("Player1Win");
+
+        if(timer == 0 && player1Points > player2Points){
+            SceneManager.LoadScene("Player2Win");
+        }
     }
 
     private void ChangePlantPoints()
@@ -37,6 +51,7 @@ public class GameManager : MonoBehaviour
         {
             FindNewPlantPoints();
             UpdatePlantPoints();
+            TextAnimation();
             timerSaved -= 30;
         }
     }
@@ -65,5 +80,46 @@ public class GameManager : MonoBehaviour
         {
             redPlantPoints= (int)Random.Range(1, 4); ;
         }
+    }
+
+
+    private void TextAnimation(){
+        changePointText.gameObject.SetActive(true);
+        StartCoroutine(ShowLine());
+    }
+
+    private void StartAnimation(){
+        startText.gameObject.SetActive(true);
+        StartCoroutine(hideStart());
+    }
+
+    private IEnumerator ShowLine(){
+
+        changePointText.text = string.Empty;
+        foreach(char ch in "CHANGE OF POINTS PER PLANT"){
+            changePointText.text += ch;
+            yield return new WaitForSeconds(0.1f);
+        }
+        StartCoroutine(hideLine());
+    }
+
+    private IEnumerator showStart(){
+        startText.text = string.Empty;
+        foreach(char ch in "START!!!"){
+            changePointText.text += ch;
+            yield return new WaitForSeconds(2f);
+        }
+        StartCoroutine(hideStart());
+    }
+
+    private IEnumerator hideLine(){
+        yield return new WaitForSeconds(3f);
+        changePointText.gameObject.SetActive(false);
+    }
+
+    private IEnumerator hideStart(){
+        yield return new WaitForSeconds(3f);
+        startText.gameObject.SetActive(false);
+        
     }
 }
